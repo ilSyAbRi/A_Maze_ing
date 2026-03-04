5d785f5ee1ea605ff9ea86e251bfbacbb5664c2a
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-04 22:06:14 +0000

    protect file no find

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -1,1 +1,8 @@
-we start parsing
+def check_the_file():
+    try:
+        x = open("config.txt","r")
+    except FileNotFoundError:
+        print("Hey! I can’t find config.txt. Did you move it or rename it?")
+
+if __name__ == "__main__":
+    check_the_file()
