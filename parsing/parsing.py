390657b35c4374fe89c72643eac38eaf30f652e8
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-06 08:55:26 +0000

    check file if exist and check the argument i work whit

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -1,8 +1,29 @@
-def check_the_file():
+import sys
+
+
+def check_file_exist():
+    """Try to open 'config.txt' and read it."""
+    if len(sys.argv) != 2:
+        print("ONLY 2 ARGUMENT HERE NEGA")
+        sys.exit(1)
+    if sys.argv[1] != "config.txt":
+        print("argv[1] : I AM NOT config.txt AND I SHOULD BE")
+        sys.exit(1)
     try:
-        x = open("config.txt","r")
+        with open("config.txt", "r") as file:
+            content = file.read()
+            print("File read successfully!")
+            return content
     except FileNotFoundError:
-        print("Hey! I can’t find config.txt. Did you move it or rename it?")
+        print("Error: config.txt file not found!")
+        sys.exit(1)
+    except PermissionError:
+        print("Error: Permission denied for config.txt!")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Other error: {e}")
+        sys.exit(1)
+
 
 if __name__ == "__main__":
-    check_the_file()
+    check_file_exist()
