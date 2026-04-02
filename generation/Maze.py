52d45ef8ac0bf3855a0feb4a954a17ce9bdf4037
Ibrahim El-Lotfi <ellotfidev@gmail.com>
2026-04-02 18:58:30 +0100

    fix the calculate form to fit all the sizes

--- generation/Maze.py
+++ generation/Maze.py
@@ -5,6 +5,10 @@ class Maze:
     def calculate_cell_size(width, height):
         adjust_width = 1920 // width 
         adjust_height = 1080 // height
+        if (width * height) < 1000:
+            return min(adjust_width, adjust_height) - 10
+        if (width * height) < 5000:
+            return min(adjust_width, adjust_height) - 2
         return min(adjust_width, adjust_height)
     
     def __init__(self, width, height, entry, exit, output_file, perfect):
@@ -15,4 +19,4 @@ class Maze:
         self.output_file = output_file
         self.perfect = perfect
         self.grid = [[ Cell() for _ in range(width)] for  _ in range(height)]
-        self.cell_size = Maze.calculate_cell_size(width, height) - 10
+        self.cell_size = Maze.calculate_cell_size(width, height)
