d1a24302abd3f6ddea3d7bed4c1b7e02f5c2071d
ilSyAbRi <iliasssyabri1@gmail.com>
2026-04-03 11:10:09 +0100

    do the direction and dfs algo start appear

--- generation/Maze.py
+++ generation/Maze.py
@@ -20,3 +20,56 @@ class Maze:
         self.perfect = perfect
         self.grid = [[ Cell() for _ in range(width)] for  _ in range(height)]
         self.cell_size = Maze.calculate_cell_size(width, height)
+
+    def get_unvisited_neighbors(self, x, y):
+        neighbors = []
+        # UP
+        if x > 0:
+            if not self.grid[x - 1][y].visited:
+                neighbors.append((x - 1, y, "N"))
+        # down
+        if x < self.height - 1 :
+            if not self.grid[x + 1][y].visited:
+                neighbors.append((x + 1, y, "S"))
+        # lefth
+        if y > 0:
+            if not self.grid[x][y - 1].visited:
+                neighbors.append((x, y - 1, "W"))
+        # right
+        if y < self.width - 1:
+            if not self.grid[x][y + 1].visited:
+                neighbors.append((x, y + 1, "E"))
+        return neighbors
+
+    def do_dfs_algo(self):
+        stack = [self.entry]
+        path = [self.entry]
+        self.grid[self.entry[0]][self.entry[1]].visited = True
+
+        while stack:
+            x, y = stack[-1]
+            neighbors = self.get_unvisited_neighbors(x, y)
+
+            if neighbors:
+                nx, ny, direction = random.choice(neighbors)
+                if direction == "N":
+                    self.grid[x][y].north = False
+                    self.grid[nx][ny].south = False
+                elif direction == "S":
+                    self.grid[x][y].south = False
+                    self.grid[nx][ny].north = False
+                elif direction == "E":
+                    self.grid[x][y].east = False
+                    self.grid[nx][ny].west = False
+                elif direction == "W":
+                    self.grid[x][y].west = False
+                    self.grid[nx][ny].east = False
+
+                self.grid[nx][ny].visited = True
+                path.append((nx, ny))
+                stack.append((nx, ny))
+            else:
+                stack.pop()
+
+        self.path = path
+        return path
