e6de15fb991c8a6d9e20ff217d91adcb5ec61c2f
ilSyAbRi <iliasssyabri1@gmail.com>
2026-04-03 17:56:00 +0100

    the process of working

--- generation/Generator.py
+++ generation/Generator.py
@@ -1,14 +1,75 @@
-from parsing.parsing import config_parser
-import mlx
+from generation.Maze import Maze
+import random
+
+
+class Cell:
+    def __init__(self):
+        self.north = True
+        self.east = True
+        self.south = True
+        self.west = True
+        self.visited = False
+
 
 class MazeGenerator:
-    @staticmethod 
-    def generate_maze(config_file: str):
-        config = config_parser(config_file)
-        width = config['width']
-        height = config['height']
-        # Here you would implement your maze generation algorithm
-        # For demonstration, we will just create a simple maze structure
-        maze = [[0 for _ in range(width)] for _ in range(height)]
-        # You can add walls and paths to the maze as needed
-        return maze
+    def __init__(self, maze: Maze):
+        self.maze = maze
+        self.grid = maze.grid
+        self.width = maze.width
+        self.height = maze.height
+        self.entry = maze.entry
+
+    def get_unvisited_neighbors(self, row, col):
+        neighbors = []
+
+        if row > 0 and not self.grid[row - 1][col].visited:
+            neighbors.append((row - 1, col, "N"))
+
+        if row < self.height - 1 and not self.grid[row + 1][col].visited:
+            neighbors.append((row + 1, col, "S"))
+
+        if col > 0 and not self.grid[row][col - 1].visited:
+            neighbors.append((row, col - 1, "W"))
+
+        if col < self.width - 1 and not self.grid[row][col + 1].visited:
+            neighbors.append((row, col + 1, "E"))
+
+        return neighbors
+
+    def generate_maze(self):
+        stack = [self.entry]
+        path = [self.entry]
+
+        row, col = self.entry
+        self.grid[row][col].visited = True
+
+        while stack:
+            row, col = stack[-1]
+            neighbors = self.get_unvisited_neighbors(row, col)
+
+            if neighbors:
+                if self.perfect.lower() == "true":
+                    random.seed(self.seed)
+                else:
+                    nx, ny, direction = random.choice(neighbors)
+
+                if direction == "N":
+                    self.grid[row][col].north = False
+                    self.grid[nx][ny].south = False
+                elif direction == "S":
+                    self.grid[row][col].south = False
+                    self.grid[nx][ny].north = False
+                elif direction == "E":
+                    self.grid[row][col].east = False
+                    self.grid[nx][ny].west = False
+                elif direction == "W":
+                    self.grid[row][col].west = False
+                    self.grid[nx][ny].east = False
+
+                self.grid[nx][ny].visited = True
+                stack.append((nx, ny))
+                path.append((nx, ny))
+            else:
+                stack.pop()
+
+        return path
