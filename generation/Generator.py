ff8b66725efbbbcea599722252242a53bece320a
ilSyAbRi <iliasssyabri1@gmail.com>
2026-04-05 20:27:50 +0100

    still the code have problem but i organized more

--- generation/Generator.py
+++ generation/Generator.py
@@ -21,40 +21,56 @@ class MazeGenerator:
 
         return neighbors
 
+
+    @staticmethod
+    def check_direction(maze, row, col, direction, nx, ny):
+        
+        if is_perfect:
+            if direction == "N":
+                maze.grid[row][col].north = False
+                maze.grid[nx][ny].south = False
+
+            elif direction == "S":
+                maze.grid[row][col].south = False
+                maze.grid[nx][ny].north = False
+
+            elif direction == "E":
+                maze.grid[row][col].east = False
+                maze.grid[nx][ny].west = False
+
+            elif direction == "W":
+                maze.grid[row][col].west = False
+                maze.grid[nx][ny].east = False
+
     @staticmethod
     def generate_maze(maze):
         stack = [maze.entry]
         path = [maze.entry]
 
-        row, col = maze.entry 
+        row, col = maze.entry
         maze.grid[row][col].visited = True
-        #seed_value = maze.seed()
-        #if seed_value is not None:
-            #random.seed(seed_value)
+
         while stack:
             row, col = stack[-1]
-            neighbors = MazeGenerator.get_unvisited_neighbors(maze,row, col)
+            neighbors = MazeGenerator.get_unvisited_neighbors(maze, row, col)
             if neighbors:
-
                 nx, ny, direction = random.choice(neighbors)
                 maze.grid[nx][ny].visited = True
-
-                if direction == "N":
-                    maze.grid[row][col].north = False
-                    maze.grid[nx][ny].south = False
-                elif direction == "S":
-                    maze.grid[row][col].south = False
-                    maze.grid[nx][ny].north = False
-                elif direction == "E":
-                    maze.grid[row][col].east = False
-                    maze.grid[nx][ny].west = False
-                elif direction == "W":
-                    maze.grid[row][col].west = False
-                    maze.grid[nx][ny].east = False
-
+                MazeGenerator.check_direction(maze, row, col, direction, nx, ny,True)
                 stack.append((nx, ny))
                 path.append((nx, ny))
             else:
                 stack.pop()
 
         return path
+        
+        if maze.perfect.lower() == "false":
+            row, col = maze.entry
+            neighbors = MazeGerator.get_unvisited_neighbors(maze,row,col)
+            x,y,direction = random.choice(neighbors)
+            check_direction(maze,row,col,direction,row + 1,col + 1,False)
+            row , col = maze.exit
+            neighbors = MazeGenerator.get_unvisited_neighbors(maze,row,col)
+            x, y, direction = random.choice(neighbors)
+            check_direction = random.choice(neighbors)
+
